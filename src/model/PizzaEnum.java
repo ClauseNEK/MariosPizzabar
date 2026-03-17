86f45a2efe56ee177d27145625f0f379f79d17c7
ChefenML <milo1000.stud.ek.dk>
2026-03-17 11:18:17 +0100

    PizzaEnum all pizza added

--- src/model/PizzaEnum.java
+++ src/model/PizzaEnum.java
@@ -7,7 +7,16 @@ public enum PizzaEnum {
     Cacciatore (3,"tomatsauce, ost, pepperoni og oregano",57),
     Carbonara (4, "tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano",63),
     Dennis(5,"tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano",65),
-    Berti(6,"tomatsauce, ost, bacon og oregano",57)
+    Berti(6,"tomatsauce, ost, bacon og oregano",57),
+    Silvia(7, "tomatsauce, ost, pepperoni, rød peber, løg, oliven og oregano", 61),
+    Victoria(8,"tomatsauce, ost, skinke, ananas, champignon, løg og oregano",61),
+    Torno(9,"tomatsauce, ost, skinke, bacon, kebab, chili og oregano",61),
+    Capricciosa(10," tomatsauce, ost, skinke, champignon og oregano",61),
+    Hawaii(11,"tomatsauce, ost, skinke, ananas og oregano",61),
+    Le_Blisso(12,"tomatsauce, ost, skinke, rejer og oregano",61),
+    Venezia(13,"tomatsauce, ost, skinke, bacon og oregano",61),
+    Mafia(14,"tomatsauce, ost, pepperoni, bacon, løg og oregano",61);
+
     // Field (variable) to store the description text
     private String description;
     private double price;
