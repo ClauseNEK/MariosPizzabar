7d912c06f71612fbf6879be78ad32c8bf77678f9
ChefenML <milo1000.stud.ek.dk>
2026-03-17 11:13:30 +0100

    PizzaEnum double

--- src/model/PizzaEnum.java
+++ src/model/PizzaEnum.java
@@ -2,16 +2,20 @@ package model;
 
 public enum PizzaEnum {
     // Enum constants (each has its own description)
-    Vesuvius("tomatsauce, ost, skinke og oregano", 57),
-    Amerikaner ("tomatsauce, ost, oksefars og oregano",53),
-    Cacciatore ("tomatsauce, ost, pepperoni og oregano",57);
-
+    Vesuvius(1,"tomatsauce, ost, skinke og oregano", 57),
+    Amerikaner (2,"tomatsauce, ost, oksefars og oregano",53),
+    Cacciatore (3,"tomatsauce, ost, pepperoni og oregano",57),
+    Carbonara (4, "tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano",63),
+    Dennis(5,"tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano",65),
+    Berti(6,"tomatsauce, ost, bacon og oregano",57)
     // Field (variable) to store the description text
     private String description;
     private double price;
+    private int id;
 
     // Constructor (runs once for each constant above)
-    private PizzaEnum(String description, int price) {
+    private PizzaEnum(int id, String description, int price) {
+        this.id = id;
         this.description = description;
         this.price = price;
     }
@@ -24,5 +28,10 @@ public enum PizzaEnum {
     public double getPrice() {
         return price;
     }
+
+    public int getId() {
+        return id;
+    }
+
 }
 
