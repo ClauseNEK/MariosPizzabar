9ebdf6a25dc9aa203b840311b77def719948af7d
ChefenML <milo1000.stud.ek.dk>
2026-03-19 11:10:03 +0100

    SortBy: name, price, ID  & description

--- src/util/PizzaSorter.java
+++ src/util/PizzaSorter.java
@@ -1,4 +1,27 @@
 package util;
 
+import model.Pizza;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class PizzaSorter {
+
+    public static void sortByName(ArrayList<Pizza> pizza){
+        Collections.sort(pizza, Comparator.comparing(Pizza::getName));
+    }
+
+    public static void sortByPrice(ArrayList<Pizza> pizza){
+        Collections.sort(pizza, Comparator.comparingDouble(Pizza::getPrice));
+    }
+
+    public static void sortByDescription(ArrayList<Pizza> pizza){
+
+        Collections.sort(pizza, Comparator.comparing(Pizza::getDescription));
+    }
+
+    public static void sortById(ArrayList<Pizza> pizza){
+        Collections.sort(pizza, Comparator.comparingInt(Pizza::getId));
+    }
 }
