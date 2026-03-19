3a8b1a6a0d50006a1b4b04e102e1552ee41c21b5
silvurstein <sveinursilvurstein@gmail.com>
2026-03-19 11:22:40 +0100

    PizzaOrderClass comments og rabat beregning

--- src/model/PizzaOrderClass.java
+++ src/model/PizzaOrderClass.java
@@ -30,10 +30,36 @@ public class PizzaOrderClass {
     public LocalTime getPickupTime() {
         return pickupTime;
     }
+    // Beregner samlede pris før rabat.
+    public double getSubtotal(){
+        double subtotal = 0.0;
+
+        //lægger prisen fra alle pizzaer sammen.
+        for (Pizza pizza : pizzas){
+            subtotal += pizza.getPrice();
+        }
+        return subtotal;
+    }
+
+    //beregner hvor meget rabat kundetypen få.
+    public double getDiscountAmount(){
+        return getSubtotal() * customer.getDiscount();
+    }
+
+    //Beregner den endelig pris efter rabat
+    public double getTotalPrice(){
+        return getSubtotal() - getDiscountAmount();
+    }
 
     @Override
     public String toString() {
-        return "OrderID: " + orderId + " " + "Kunde: " + customer.getName() + " " + "Telefon: " + customer.getPhoneNumber()
-            + " " + "Afhentning: " + pickupTime;   // Pizza's udskrives for sig
+        return "OrderID: " + orderId
+                + "Kunde: " + customer.getName()
+                + "Telefon: " + customer.getPhoneNumber()
+                + "Afhentning: " + pickupTime   // Pizza's udskrives for sig
+                + "Subtotal: " + getSubtotal()
+                + "Rabat: " + getDiscountAmount()
+                + "Endelig pris: " + getTotalPrice();
+
     }
 }
