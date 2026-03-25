5223990f38db377f4d4f39c5b8c1284cdd9cf996
ChefenML <milo1000.stud.ek.dk>
2026-03-25 16:06:54 +0100

    Comments

--- src/main/PizzaApp.java
+++ src/main/PizzaApp.java
@@ -1,18 +1,43 @@
 package main;
 import file.FileHandler;
+import model.PizzaArray;
 import ui.PizzaUI;
+
+import java.io.File;
+import java.util.Arrays;
 
 public class PizzaApp {
     public static void main(String[] args) {
         PizzaUI pizzaUI = new PizzaUI();
+        PizzaArray pizzaArray = new PizzaArray();
         pizzaUI.start();
 
         //temporary test:
         System.out.println("orders: " + FileHandler.countAllOrders());
-        System.out.println("Pizza Count: " + FileHandler.countPizzaSales());
+        System.out.println("Pizza sold by pizza#: " + Arrays.toString(FileHandler.countPizzaSales()));
         System.out.println("Best seller: " + FileHandler.findBestSeller());
-
-        pizzaUI.pizzaAschiiArt1();
-        pizzaUI.pizzaAschiiArt2();
+        //pizzaUI.pizzaAschiiArt1();
+        //pizzaUI.pizzaAschiiArt2();
     }
 }
+//Solgte:
+
+//Vesuvius:     0
+//Amerikaner:   1
+//Cacciatore:   2
+//Carbonara:    3
+//Dennis:       4
+//Berti:        5
+//Silvia:       6
+//Victoria:     6
+//Torno:        7
+//Capricciosa:  8
+//Hawaii:       9
+//Le_Blisso:    9
+//Venezia:      7
+//Mafia:        5
+//eller bare sortering af hele listen?
+//Bestseller1: navn + antal
+//Bestseller2: navn + antal
+//Bestseller3: navn + antal
+
