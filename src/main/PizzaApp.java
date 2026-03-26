60bd8749b936440e153ba0b356d03da66f792b58
silvurstein <sveinursilvurstein@gmail.com>
2026-03-26 09:11:30 +0100

    pizza app remove temporary

--- src/main/PizzaApp.java
+++ src/main/PizzaApp.java
@@ -12,33 +12,6 @@ public class PizzaApp {
         PizzaArray pizzaArray = new PizzaArray();
         pizzaUI.start();
 
-        //temporary test:
-        System.out.println("orders: " + FileHandler.countAllOrders());
-        System.out.println("Pizza sold by pizza#: " + Arrays.toString(FileHandler.countPizzaSales()));
-
-
-        //pizzaUI.pizzaAschiiArt1();
-        //pizzaUI.pizzaAschiiArt2();
     }
 }
-//Solgte:
-
-//Vesuvius:     0
-//Amerikaner:   1
-//Cacciatore:   2
-//Carbonara:    3
-//Dennis:       4
-//Berti:        5
-//Silvia:       6
-//Victoria:     6
-//Torno:        7
-//Capricciosa:  8
-//Hawaii:       9
-//Le_Blisso:    9
-//Venezia:      7
-//Mafia:        5
-//eller bare sortering af hele listen?
-//Bestseller1: navn + antal
-//Bestseller2: navn + antal
-//Bestseller3: navn + antal
 
